9a922c2f086b85e7bd048a355970382cfe72c144
krishpranav <krisna.pranav@gmail.com>
2021-06-25 16:16:35 +0530

    [+] Wam Web View [+]

--- WeChat/WAMWebView.m
+++ WeChat/WAMWebView.m
@@ -6,4 +6,22 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "WAMWebView.h"
+
+@implementation WAMWebView
+
+- (BOOL)performKeyEquivalent:(NSEvent *)theEvent {
+    BOOL superResult = [super performKeyEquivalent:theEvent];
+    switch (theEvent.keyCode) {
+        case 53:        // esc key
+        case 124:       // right arrow
+        case 123:       // left arrow
+            return YES;
+            break;
+        default:
+            return superResult;
+            break;
+    }
+}
+
+@end
