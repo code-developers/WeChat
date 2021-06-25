3f524212a483d5411ce276f8bf511a8731e94b62
krishpranav <krisna.pranav@gmail.com>
2021-06-25 16:08:20 +0530

    [+] WAM Application [+]

---
+++ WeChat/WAMApplication.h
@@ -0,0 +1,13 @@
+//
+//  WAMApplication.h
+//  WeChat
+//
+//  Created by Elangovan Ayyasamy on 25/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef WAMApplication_h
+#define WAMApplication_h
+
+
+#endif /* WAMApplication_h */
