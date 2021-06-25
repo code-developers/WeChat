b0a372ed1861e7fbc08a61f6fd14aa6dd93c833f
krishpranav <krisna.pranav@gmail.com>
2021-06-25 16:23:17 +0530

    [+] WK Web View Private [+]

--- WeChat/WKWebView+Private.h
+++ WeChat/WKWebView+Private.h
@@ -6,3 +6,8 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
+#import <WebKit/WebKit.h>
+
+@interface WKWebView (Private)
+@property (copy, setter=_setCustomUserAgent:) NSString *_customUserAgent;
+@end 
