232259205a92e1a0a850d0bf7dc16048e023ce7a
krishpranav <krisna.pranav@gmail.com>
2021-06-25 16:14:47 +0530

    [+] WAM Web View [+]

---
+++ WeChat/WAMWebView.m
@@ -0,0 +1,9 @@
+//
+//  WAMWebView.m
+//  WeChat
+//
+//  Created by Elangovan Ayyasamy on 25/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
