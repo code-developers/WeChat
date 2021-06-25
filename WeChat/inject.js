5be3d74f339ca6934c1f532114dfda7371ebec14
krishpranav <krisna.pranav@gmail.com>
2021-06-25 16:02:20 +0530

    [+] notification [+]

--- WeChat/inject.js
+++ WeChat/inject.js
@@ -2,3 +2,9 @@ jQuery.noConflict();
 jQuery(document).on('click', 'input[type="file"]', function () {
     alert('To upload media, drag and drop the file into the WhatsApp Web window.');
 });
+
+this.Notification = function (title, options) {
+    webkit.messageHandlers.notification.postMessage([title, options.body, options.tag]);
+};
+this.Notification.permission = 'granted';
+this.Notification.requestPermission = function(callback) {callback('granted');};
